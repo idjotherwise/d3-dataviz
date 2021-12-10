ab4705ffe2e6f635ce59a0506f9c6c4300c04754
Ifanj <ifan.johnston@gmail.com>
2021-12-10 01:02:27 +0100

    working transition scatter chart

--- section3project/js/main.js
+++ section3project/js/main.js
@@ -4,9 +4,11 @@
 *    Project 1 - Star Break Coffee
 */
 
-const MARGIN = {LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100}
+const MARGIN = {LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 120}
 const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+
+let flag = true
 
 // {month, revenue, profit}
 
@@ -16,46 +18,21 @@ const g = d3.select("#chart-area").append("svg")
 .append("g")
 .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
-
-
-
-d3.csv("data/revenues.csv").then(data => {
-  data.forEach(d => {
-
-    d.revenue= Number(d.revenue)
-    d.profit= Number(d.profit)
-  })
-
-  const x = d3.scaleBand()
-  .domain(data.map(d => d.month))
+const x = d3.scaleBand()
   .range([0, WIDTH])
   .paddingInner(0.2)
   .paddingOuter(0.2)
 
   const y = d3.scaleLinear()
-  .domain([0, d3.max(data, d => d.profit)])
   .range([HEIGHT, 0])
 
-  const xAxisCall = d3.axisBottom(x)
-  g.append("g")
-  .attr("transform", `translate(0, ${HEIGHT})`)
-  .call(xAxisCall)
-
-  
-  const yAxisCall = d3.axisLeft(y)
-  .ticks(4)
-  .tickFormat(d=>"£"+d)
-  
-  g.append("g")
-  .call(yAxisCall)
-  
-  g.append("text")
+  yLabel = g.append("text")
   .attr("x", -(HEIGHT / 2))
   .attr("y", -60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
-  .text("Revenue (£)")
+  
   
   g.append("text")
   .attr("x", WIDTH / 2)
@@ -64,13 +41,73 @@ d3.csv("data/revenues.csv").then(data => {
   .attr("text-anchor", "middle")
   .text("Month")
 
-  const rects = g.selectAll("rect").data(data)
+  const xAxisGroup = g.append("g")
+  .attr("transform", `translate(0, ${HEIGHT})`)
 
+  const yAxisGroup = g.append("g")
 
+d3.csv("data/revenues.csv").then(data => {
+  data.forEach(d => {
+    d.revenue= Number(d.revenue)
+    d.profit= Number(d.profit)
+  })
 
-  rects.enter().append("rect")
-  .attr("height", (d, i) => (HEIGHT - y(d.profit)))
-  .attr("width", x.bandwidth)
-  .attr("x", d => x(d.month))
-  .attr("y", d => y(d.profit))
+  d3.interval(() => {
+    flag = !flag
+    const newData = flag ? data : data.slice(1)
+    update(newData)
+  }, 1000)
+
+  update(data)
 })
+
+function update (data) {
+  const value = flag ? "profit" : "revenue"
+  const t = d3.transition().duration(750)
+
+  x.domain(data.map(d => d.month))
+  y.domain([0, d3.max(data, d => d[value])])
+
+  const xAxisCall = d3.axisBottom(x)
+  xAxisGroup.transition(t).call(xAxisCall)
+  .selectAll("text")
+  .attr("y", "10")
+  .attr("x", "-20")
+  .attr("transform", "rotate(-40)")
+
+  
+  const yAxisCall = d3.axisLeft(y)
+  .ticks(4)
+  .tickFormat(d=>"£"+d)
+  
+  yAxisGroup.transition(t).call(yAxisCall)
+  
+  
+  // JOIN new data with old elements
+  const circles = g.selectAll("circle").data(data, d => d.month)
+
+  // EXIT old elements not present in new data
+  circles.exit()
+  .attr("fill", "red")
+  .transition(t)
+  .attr("height", 0)
+  .attr("cy", y(0))
+  .remove()
+
+  // ENTER new elemenets present in new data
+  circles.enter().append("circle")
+  .attr("fill", "grey")
+  .attr("cy", y(0))
+  .attr("r", 5)
+  // AND UPDATE
+  .merge(circles)
+  .transition(t)
+    .attr("fill-opacity", 1)
+    .attr("cx", d => x(d.month) + x.bandwidth() / 2)
+    .attr("cy", d=> y(d[value]))
+  
+    //.attr("fill-opacity", 0)
+
+  const text = flag ? "Profit ($)" : "Revenue($)"
+  yLabel.text(text)
+}
